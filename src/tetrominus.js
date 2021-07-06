75927c2df5383d10f365ba74e5ecaa2cec571a32
Trey <treywroberts@gmail.com>
2021-07-06 16:05:40 -0400

    setup custom hooks for usePlayer and useStage

--- src/tetrominus.js
+++ src/tetrominus.js
@@ -1,7 +1,7 @@
 export const TETROMINOS = {
     0: { shape: [[0]], color:'0,0,0'},
     I: {
-        Shape: [
+        shape: [
             [0, 'I', 0, 0],
             [0, 'I', 0, 0],
             [0, 'I', 0, 0],
@@ -10,7 +10,7 @@ export const TETROMINOS = {
         color:'80, 227, 230'
     },
     J: {
-        Shape: [
+        shape: [
             [0, 'J', 0],
             [0, 'J', 0],
             ['J', 'J', 0],
@@ -18,7 +18,7 @@ export const TETROMINOS = {
         color:'36, 95, 223'
     },
     L: {
-        Shape: [
+        shape: [
             [0, 'L', 0],
             [0, 'L', 0],
             [0, 'L', 'L'],
@@ -26,14 +26,14 @@ export const TETROMINOS = {
         color:'223, 173, 36'
     },
     O: {
-        Shape: [
+        shape: [
             ['O', 'O'],
             ['O', 'O'],
         ],
         color:'223, 217, 36'
     },
     S: {
-        Shape: [
+        shape: [
             [0, 'S', 'S'],
             ['S', 'S', 0],
             [0, 0, 0],
@@ -41,7 +41,7 @@ export const TETROMINOS = {
         color:'48, 211, 56'
     },
     T: {
-        Shape: [
+        shape: [
             [0, 0, 0],
             ['T', 'T', 'T'],
             [0, 'T', 0],
@@ -49,7 +49,7 @@ export const TETROMINOS = {
         color:'132, 61, 198'
     },
     Z: {
-        Shape: [
+        shape: [
             ['Z', 'Z', 0],
             [0, 'Z', 'Z'],
             [0, 0, 0],
@@ -61,6 +61,6 @@ export const TETROMINOS = {
 export const randomTetromino = () =>{
     const tetrominos = 'IJLOSTZ'
     const randTetro = 
-    tetrominos[Math.floor(Math.random() + tetrominos.length)]
+    tetrominos[Math.floor(Math.random() * tetrominos.length)]
     return TETROMINOS[randTetro]
 }
